d7fe3132986042d50e31bc86e0ceab723858d912
Sebin Suresh <sebinsuresh@gmail.com>
2026-03-24 18:20:02 -0500

    phase 1 and 2

--- public/background.js
+++ public/background.js
@@ -1,26 +1,3 @@
-chrome.runtime.onInstalled.addListener((details) => { 
-  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-    chrome.tabs.create({
-      url: "https://kamilrizatdinov.github.io/reader_line/welcome",
-    });
-  } else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) { 
-    // When extension is updated
-  } else if ( details.reason === chrome.runtime.OnInstalledReason.CHROME_UPDATE) {
-    // When browser is updated
-  } else if (details.reason === chrome.runtime.OnInstalledReason.SHARED_MODULE_UPDATE) {
-    // When a shared module is updated
-  }
-});
-
-chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
-  if (message.type && message.type === 'openPopup') {
-    chrome.action.openPopup()
-    sendResponse({status: "OK"})
-  }
-})
-
-chrome.runtime.setUninstallURL("https://kamilrizatdinov.github.io/reader_line/goodbye");
-
 
 class TabsController {
   tabsWithLoadedSidebar= {}
